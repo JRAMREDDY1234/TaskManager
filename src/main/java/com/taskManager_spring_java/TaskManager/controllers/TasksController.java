f5444c9bb3e725057dcedef39ffcda7ac00176f6
Ram <ramjagathpally@gmail.com>
2025-03-20 17:10:30 +0530

    third commit validating date and adding the edit feature to the tasks

--- src/main/java/com/taskManager_spring_java/TaskManager/controllers/TasksController.java
+++ src/main/java/com/taskManager_spring_java/TaskManager/controllers/TasksController.java
@@ -1,11 +1,14 @@
 package com.taskManager_spring_java.TaskManager.controllers;
 
 import com.taskManager_spring_java.TaskManager.dtos.CreateTaskDTO;
+import com.taskManager_spring_java.TaskManager.dtos.ErrorResponseDTO;
+import com.taskManager_spring_java.TaskManager.dtos.UpdateTaskDTO;
 import com.taskManager_spring_java.TaskManager.entities.TaskEntity;
 import com.taskManager_spring_java.TaskManager.repositories.TaskService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.ParseException;
 import java.util.List;
 
 @RestController
@@ -35,9 +38,29 @@ public class TasksController {
     }
 
     @PostMapping("")
-    public ResponseEntity<TaskEntity> addTask(@RequestBody CreateTaskDTO body){
+    public ResponseEntity<TaskEntity> addTask(@RequestBody CreateTaskDTO body) throws ParseException {
         var task = taskService.addTask(body.getTitle(), body.getDescription(), body.getDeadline());
 
         return ResponseEntity.ok(task);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<TaskEntity> updateTask(@PathVariable("id") Integer id, @RequestBody UpdateTaskDTO body) throws ParseException{
+        var task = taskService.updateTask(id, body.getDescription(), body.getDeadline(), body.getCompleted());
+
+        if(task ==null){
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(task);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDTO> handleErrors(Exception e){
+        if(e instanceof ParseException){
+            return ResponseEntity.badRequest().body(new ErrorResponseDTO("Invalid Date format"));
+        }
+        e.printStackTrace();
+        return ResponseEntity.internalServerError().body((new ErrorResponseDTO("Internal Server Error")));
     }
 }
